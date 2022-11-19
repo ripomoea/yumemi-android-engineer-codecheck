b29734c9e4be7ce2a14158f6218b4f1d41360ed8
Ryo Takeuchi <develop@r09i.me>
2022-11-19 20:09:31 +0900

    :wrench: アプリケーション用のプラグインを追加
    アプリケーション用のビルドロジックプラグインを追加しました。

---
+++ build-logic/src/main/kotlin/jp/co/yumemi/android/codecheck/KotlinAndroid.kt
@@ -0,0 +1,38 @@
+package jp.co.yumemi.android.codecheck
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.api.plugins.ExtensionAware
+import org.gradle.kotlin.dsl.dependencies
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+
+internal fun Project.configureKotlinAndroid(
+    commonExtension: CommonExtension<*, *, *, *>,
+) {
+    commonExtension.apply {
+        compileSdk = 31
+
+        defaultConfig {
+            minSdk = 23
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+            isCoreLibraryDesugaringEnabled = true
+        }
+
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_1_8.toString()
+        }
+    }
+
+    dependencies {
+        add("coreLibraryDesugaring", libs.findLibrary("android.desugarJdkLibs").get())
+    }
+}
+
+fun CommonExtension<*, *, *, *>.kotlinOptions(block: KotlinJvmOptions.() -> Unit) {
+    (this as ExtensionAware).extensions.configure("kotlinOptions", block)
+}
