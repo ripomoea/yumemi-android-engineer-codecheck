a5b8c999a806fb2907b300da43a45987c705b086
Ryo Takeuchi <develop@r09i.me>
2022-11-21 20:25:50 +0900

    :recycle: リポジトリ関連のクラスをリファクタリング
    UI 表示改善のためにリポジトリについて扱うデータを見直ししました。
    併せて不要な記述等の削除を含めたリファクタリングも実施しています。
    
    - パラメータの追加
      - id
      - description
      - owner > login
    - 必須で返却されるパラメータのデフォルト値を削除

--- core/network/src/main/java/jp/co/yumemi/android/codecheck/core/network/model/NetworkGithubRepo.kt
+++ core/network/src/main/java/jp/co/yumemi/android/codecheck/core/network/model/NetworkGithubRepo.kt
@@ -14,30 +14,39 @@ data class SearchGithubReposResponse internal constructor(
 
 @Serializable
 data class NetworkGithubRepo internal constructor(
+    @SerialName(value = "id")
+    val id: Long,
+
     @SerialName(value = "name")
     val name: String = "",
 
     @SerialName(value = "owner")
     val owner: NetworkRepositoryOwner? = null,
 
+    @SerialName(value = "description")
+    val description: String,
+
     @SerialName(value = "language")
     val language: String? = null,
 
     @SerialName(value = "stargazers_count")
-    val stargazersCount: Long = 0,
+    val stargazersCount: Long,
 
     @SerialName(value = "watchers_count")
-    val watchersCount: Long = 0,
+    val watchersCount: Long,
 
     @SerialName(value = "forks_count")
-    val forksCount: Long = 0,
+    val forksCount: Long,
 
     @SerialName(value = "open_issues_count")
-    val openIssuesCount: Long = 0,
+    val openIssuesCount: Long,
 )
 
 @Serializable
 data class NetworkRepositoryOwner internal constructor(
+    @SerialName(value = "login")
+    val login: String,
+
     @SerialName(value = "avatar_url")
-    val iconUrl: String = "",
+    val iconUrl: String,
 )
