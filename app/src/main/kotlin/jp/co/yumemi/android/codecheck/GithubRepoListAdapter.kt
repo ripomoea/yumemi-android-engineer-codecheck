f8b8001ffebd8dc87b8609f7463aeee3c52ae285
Ryo Takeuchi <develop@r09i.me>
2022-11-21 17:56:10 +0900

    :recycle: リポジトリ検索画面の RecyclerView 関連の処理をリファクタリング
    リポジトリ検索画面の RecyclerView 関連の処理をリファクタリングしました。

---
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/GithubRepoListAdapter.kt
@@ -0,0 +1,62 @@
+package jp.co.yumemi.android.codecheck
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import jp.co.yumemi.android.codecheck.core.model.GithubRepo
+import jp.co.yumemi.android.codecheck.databinding.ItemGithubrepoListBinding
+
+internal class GithubRepoListAdapter(
+    private val onGithubRepoClick: (GithubRepo) -> Unit,
+) : ListAdapter<GithubRepo, GithubRepoViewHolder>(githubRepoDiffCallback) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GithubRepoViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_githubrepo_list, parent, false)
+        return GithubRepoViewHolder(view, onGithubRepoClick)
+    }
+
+    override fun onBindViewHolder(holder: GithubRepoViewHolder, position: Int) {
+        holder.bind(githubRepo = getItem(position))
+    }
+
+    override fun onViewRecycled(holder: GithubRepoViewHolder) {
+        holder.unbind()
+    }
+}
+
+internal class GithubRepoViewHolder(
+    view: View,
+    onGithubRepoClick: (GithubRepo) -> Unit,
+) : RecyclerView.ViewHolder(view) {
+    private val binding = ItemGithubrepoListBinding.bind(view)
+    private var currentGithubRepo: GithubRepo? = null
+
+    init {
+        view.setOnClickListener {
+            currentGithubRepo?.let(onGithubRepoClick)
+        }
+    }
+
+    internal fun bind(githubRepo: GithubRepo) {
+        currentGithubRepo = githubRepo
+
+        binding.repositoryNameView.text = githubRepo.name
+    }
+
+    internal fun unbind() {
+        currentGithubRepo = null
+    }
+}
+
+private val githubRepoDiffCallback = object : DiffUtil.ItemCallback<GithubRepo>() {
+    override fun areItemsTheSame(oldItem: GithubRepo, newItem: GithubRepo): Boolean {
+        return oldItem.name == newItem.name
+    }
+
+    override fun areContentsTheSame(oldItem: GithubRepo, newItem: GithubRepo): Boolean {
+        return oldItem == newItem
+    }
+}
--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/GithubRepoSearchFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/GithubRepoSearchFragment.kt
@@ -4,20 +4,14 @@
 package jp.co.yumemi.android.codecheck
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import jp.co.yumemi.android.codecheck.core.model.GithubRepo
 import jp.co.yumemi.android.codecheck.databinding.FragmentGithubrepoSearchBinding
@@ -35,13 +29,9 @@ class GithubRepoSearchFragment : Fragment(R.layout.fragment_githubrepo_search) {
         val layoutManager = LinearLayoutManager(requireContext())
         val dividerItemDecoration =
             DividerItemDecoration(requireContext(), layoutManager.orientation)
-        val adapter = RepositoryListAdapter(
-            object : RepositoryListAdapter.OnItemClickListener {
-                override fun onItemClick(githubRepo: GithubRepo) {
-                    navigateToRepositoryDetailFragment(githubRepo)
-                }
-            }
-        )
+        val adapter = GithubRepoListAdapter { githubRepo ->
+            navigateToRepositoryDetailFragment(githubRepo)
+        }
 
         viewLifecycleOwner.lifecycleScope.launchWhenStarted {
             githubRepoSearchViewModel.githubRepos.collect(adapter::submitList)
@@ -64,48 +54,10 @@ class GithubRepoSearchFragment : Fragment(R.layout.fragment_githubrepo_search) {
         }
     }
 
-    fun navigateToRepositoryDetailFragment(githubRepo: GithubRepo) {
+    private fun navigateToRepositoryDetailFragment(githubRepo: GithubRepo) {
         val lastSearchDate = githubRepoSearchViewModel.lastSearchDate.value ?: return
         val action = GithubRepoSearchFragmentDirections
             .actionToGithubRepoDetailFragment(lastSearchDate, githubRepo)
         findNavController().navigate(action)
-    }
-}
-
-val diffUtil = object : DiffUtil.ItemCallback<GithubRepo>() {
-    override fun areItemsTheSame(oldItem: GithubRepo, newItem: GithubRepo): Boolean {
-        return oldItem.name == newItem.name
-    }
-
-    override fun areContentsTheSame(oldItem: GithubRepo, newItem: GithubRepo): Boolean {
-        return oldItem == newItem
-    }
-
-}
-
-class RepositoryListAdapter(
-    private val itemClickListener: OnItemClickListener,
-) : ListAdapter<GithubRepo, RepositoryListAdapter.ViewHolder>(diffUtil) {
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
-
-    interface OnItemClickListener {
-        fun onItemClick(githubRepo: GithubRepo)
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_githubrepo_list, parent, false)
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val repository = getItem(position)
-        (holder.itemView.findViewById<View>(R.id.repositoryNameView) as TextView).text =
-            repository.name
-
-        holder.itemView.setOnClickListener {
-            itemClickListener.onItemClick(repository)
-        }
     }
 }
